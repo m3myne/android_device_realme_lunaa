be7a6750deb91a9ec2c26bcd5b73fdc30574a973
m3myne <96182690+m3myne@users.noreply.github.com>
2023-05-27 22:16:02 +0500

    Initial

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_lunaa.mk
+     $(LOCAL_DIR)/miku_lunaa.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_lunaa-user \
-    aosp_lunaa-userdebug \
-    aosp_lunaa-eng
+     miku_lunaa-user \
+     miku_lunaa-userdebug \
+     miku_lunaa-eng
